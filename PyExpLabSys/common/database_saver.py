e20fa21b1a894ec744c570e559c9ea588f7acb35
Robert Jensen <jensen.robert@gmail.com>
2019-04-03 21:27:53 +0200

    Local defaults

--- PyExpLabSys/common/database_saver.py
+++ PyExpLabSys/common/database_saver.py
@@ -28,7 +28,7 @@ python2_and_3(__file__)
 
 # Database constants
 #: Hostname of the database server
-HOSTNAME = 'servcinf-sql.fysik.dtu.dk'
+HOSTNAME = '192.168.1.4'
 #: Database name
 DATABASE = 'cinfdata'
 
